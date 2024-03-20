a926535c095c296ba2f8866344da4c8c831160ac
fragoa <alirexawz81@gmail.com>
2024-03-20 18:10:26 +0330

    write register from AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+	public function register(RegisterRequest $request ) {
+
+        $validated = $request->validated();
+
+        $user = new User();
+        $user->cellphone = $validated['cellphone'];
+        $user->name = $validated['name'];
+        $user->lastname = $validated['lastname'];
+        $user->password = Hash::make($validated['password']);
+        $user->save();
+
+        return response()->json(['user' => $user]);
+
+	}
+
+	public function user() {
+	}
+}
---
+++ app/Http/Controllers/DriverSignupRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DriverSignupRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'car_plate' => ['required', 'regex:/^[1-9]{2}[بجدژسصطقلتع][1-9]{5}$/u'],
+            'car_model' => ['required'],
+        ];
+    }
+}
