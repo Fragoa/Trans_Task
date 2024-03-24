6b821807aa4842fa33820dfb01ee2dbe6ca10b6e
fragoa <alirexawz81@gmail.com>
2024-03-24 13:48:32 +0330

    fix store method

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -18,6 +18,19 @@ class TravelController extends Controller
     public function store(TravelStoreRequest $request)
     {
         $validated = $request->validated();
+//
+//        if (!$validated) {
+////            return response()->json(['errors' => $validated->toArray()->errors()], 422);
+//            return response()->json(['errors' => ['spots' => $validated->toArray()->errors()]], 422);
+//        }
+
+//        dd($validated['spots']);
+
+        // Check if the user has an active travel
+        if (Travel::userHasActiveTravel(Auth::user())) {
+            return response()->json(['code' => 'ActiveTravel'], 400);
+        }
+
 //        dd($validated);
         $travel = new Travel();
         $travel->passenger_id = Auth::id();
@@ -27,22 +40,15 @@ class TravelController extends Controller
         $spots = $validated['spots'];
 //        dd($spots);
         foreach ($spots as $spotData) {
-//            $travel->spots()->create([
-//                'position' => $spot['position'],
-//                'latitude' => $spot['latitude'],
-//                'longitude' => $spot['longitude'],
-//            ]);
             $travelSpot = new TravelSpot();
             $travelSpot->travel_id = $travel->id;
             $travelSpot->position = $spotData['position'];
             $travelSpot->latitude = $spotData['latitude'];
             $travelSpot->longitude = $spotData['longitude'];
             $travelSpot->save();
-
         }
         $travel->load('spots');
 //        dd($travel->load('spots'));
-
         return response()->json(['travel' => $travel], 201);
 
         }
